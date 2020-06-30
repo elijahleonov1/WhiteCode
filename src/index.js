01eef29e869db4f92157c3b713a94aed15396330
elijah <ex@gmail.com>
2020-06-30 15:33:04 +0300

    настройка проекта + поключение redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import rootReducer from './store/rootReducer'
+
 import './assets/styles/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
